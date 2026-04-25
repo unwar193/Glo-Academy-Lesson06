50e91dda0511361940553207e706dcb07ee44d91
Anvar Obidov <varan193@mail.ru>
2026-04-25 03:59:39 +0300

    Урок 6 финал

--- script.js
+++ script.js
@@ -4,25 +4,25 @@ const guessTheNumber = function () {
     const rightNum = Math.floor(Math.random() * 100) + 1;
     console.log(`Загаданное число ${rightNum}`);
     alert('Угадай число от 1 до 100');
+    let i = 9;
     const innerFunc = function () {
+
         const isNumber = function (num) {
         return !isNaN(parseFloat(num)) && isFinite(parseFloat(num))
         }
-        let i = 9;
-        do {
+        
+        if (i >= 0) {
             let userGuess = prompt('Введите число');
-            console.log(isNumber(userGuess));
             if (isNumber(userGuess)) {            
                 if (userGuess == rightNum) {
-                    alert('Вы угадали'); break
-                } else if (userGuess !== rightNum && userGuess < rightNum) {alert('Вы не угадали, число больше, у Вас осталось: ' + i + ' попоток!')
-                } else if (userGuess !== rightNum && userGuess > rightNum) {alert('Вы не угадали, число меньше, у Вас осталось: ' + i + ' попоток!')
-                } else {console.log(rightNum)};
-                i--;            } 
-            else if (userGuess === null) {alert(`Игра окончена`); break}
-            else {alert(`Вы ввели что-то не то, попробуйте ещё раз!`)};
-        }
-         while (i >= 0);      
+                    alert('Вы угадали');
+                } else if (userGuess !== rightNum && userGuess < rightNum) {alert('Вы не угадали, число больше, у Вас осталось: ' + i + ' попоток!'); i--; innerFunc()
+                } else if (userGuess !== rightNum && userGuess > rightNum) {alert('Вы не угадали, число меньше, у Вас осталось: ' + i + ' попоток!'); i--; innerFunc()
+                } else {};
+            } else if (userGuess === null) {alert(`Игра окончена`);
+            } else {alert(`Вы ввели что-то не то, попробуйте ещё раз!`); innerFunc()    
+            };
+        } else {alert ("Вы проиграли")};
     };
     innerFunc();
 }
